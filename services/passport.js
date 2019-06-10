84a63ac6a2604b81efebfb68c158c0f36265598e
Chris <chharvey@icloud.com>
2019-06-10 13:21:39 -0400

    initial passport&routes setup

--- services/passport.js
+++ services/passport.js
@@ -1,3 +1,18 @@
 const passport = require('passport');
 const GStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+
+passport.use(
+  new GStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile) => {
+      console.log('access token', accessToken);
+      console.log('refresh token', refreshToken);
+      console.log('profile', profile);
+    }
+  )
+);
