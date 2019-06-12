ff3cc1b3d68bcce0119ef1217c5569b1a066180b
Chris <chharvey@icloud.com>
2019-06-12 12:12:25 -0400

    cookie session

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 const routes = require('./routes/routes');
 require('./models/User');
@@ -9,6 +11,15 @@ mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 routes(app);
+
+app.use(
+  cookieSession({
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
