373946934b5b0931443273b442f2f960ac124c17
Chris <chharvey@icloud.com>
2019-06-17 18:06:21 -0400

    client proxy oath

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/auth/google"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Google Login
         </a>
       </header>
     </div>
