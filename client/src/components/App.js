248f510cfcdd8a2b52cc9853133afa91ab59df7c
Chris <chharvey@icloud.com>
2019-06-17 18:53:41 -0400

    client packages

---
+++ client/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+const App = () => {
+  return (
+    <div className="App">
+      <a
+        className="App-link"
+        href="/auth/google"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Google Login
+      </a>
+    </div>
+  );
+};
+
+export default App;
