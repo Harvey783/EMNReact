ad2ada008c6069607abd992fa4d39b6c64e82308
Chris <chharvey@icloud.com>
2019-06-17 22:32:00 -0400

    header content state

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <a href="/auth/google" className="ui black active item">
+            Login With Google
+          </a>
+        );
+      default:
+        return (
+          <ul>
+            <li>
+              <a href="/api/logout" className="ui black active item">
+                Logout
+              </a>
+            </li>
+          </ul>
+        );
+    }
+  }
+
   render() {
     return (
       <div className="ui inverted segment">
@@ -11,9 +34,7 @@ class Header extends Component {
             BUZZFEEDER
           </Link>
           <div className="ui inverted secondary menu right menu">
-            <a className="ui black active item" href="/auth/google">
-              Google Login
-            </a>
+            {this.renderContent()}
           </div>
         </div>
       </div>
@@ -21,4 +42,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
