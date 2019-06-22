8d916bbb3713b2508d4b32760f2338fbc89158a5
Chris <chharvey@icloud.com>
2019-06-22 18:04:29 -0400

    Comment Model

--- models/Comment.js
+++ models/Comment.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
-  title: String,
+const commentSchema = new Schema({
   text: String,
-  category: String,
   score: { type: Number, default: 0 },
   created: { type: Date, default: Date.now },
   updated: { type: Date, default: Date.now },
-  user: { type: Schema.Types.ObjectId, ref: 'Users' }
+  user: { type: Schema.Types.ObjectId, ref: 'Users' },
+  post: { type: Schema.Types.ObjectId, ref: 'Posts' }
 });
 
-mongoose.model('Posts', postSchema);
+mongoose.model('Comments', commentSchema);
